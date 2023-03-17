8e6a69d24d1122661d3b9d53bc440e71312a70fc
sai <sai@example.com>
2023-03-17 15:08:19 +0530

    feat:countdown

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_countdown_timer/countdown_timer_controller.dart';
 import 'package:flutter_countdown_timer/flutter_countdown_timer.dart';
+// import 'package:audioplayers/audioplayers.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, title});
@@ -16,31 +17,36 @@ class _MyHomePageState extends State<MyHomePage> {
   int hr = DateTime.now().hour;
   int min = DateTime.now().minute;
   int sec = DateTime.now().second;
-  int hrInput = 16;
-  int minInput = 6;
+  int hrInput = 14;
+  int minInput = 37;
   int secInput = 00;
-  int displayhr = 16;
-  int displaymin = 6;
+  int displayhr = 14;
+  int displaymin = 37;
   int displaysec = 00;
   int days = 0;
+  // AudioPlayer audioInput = AudioPlayer();
 
   @override
   Widget build(BuildContext context) {
     DateTime currentDuration =
         DateTime.now().add(Duration(hours: hr, minutes: min, seconds: sec));
+    //currentDuration to currentTime
     DateTime inputDuration = DateTime.now().add(Duration(
         days: days, hours: hrInput, minutes: minInput, seconds: secInput));
-    // int remainingtime = inputDuration.inSeconds - currentDuration.inSeconds;
+    //inputDuration to selctedTime
+    // int remainingTime = inputDuration.inSeconds - currentDuration.inSeconds;
 
-    Duration remainingtime = inputDuration.difference(currentDuration);
+    Duration remainingTime = inputDuration.difference(currentDuration);
+    // DateTimeRange remainingTime = DateTimeRange(start: inputDuration, end: currentDuration);
+
     print('hey');
 
     // print(inputDuration.second +
     //     inputDuration.minute * 60 +
     //     inputDuration.hour * 60 * 60);
     // print(currentDuration.second);
-    print(remainingtime);
-    // based on remainingtime.. (+ or -) resturn the scaffold
+    print(remainingTime);
+    // based on remainingTime.. (+ or -) resturn the scaffold
 
     return Scaffold(
       body: Center(
@@ -49,7 +55,7 @@ class _MyHomePageState extends State<MyHomePage> {
           Text("target-${displayhr}:${displaymin}:${displaysec}"),
           CountdownTimer(
             endTime: DateTime.now().millisecondsSinceEpoch +
-                remainingtime.inSeconds * 1000,
+                remainingTime.inSeconds * 1000,
             // DateTime.now().millisecondsSinceEpoch + 9000,
             textStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             onEnd: () {
@@ -57,9 +63,13 @@ class _MyHomePageState extends State<MyHomePage> {
               setState(() {
                 // inputDuration.add(Duration(days: 1));
 
-                days = (days + 1);
-                days = days * 60 * 60 - (remainingtime.inSeconds);
-                days = (days / 60) as int;
+                // days = (days + 1);
+                hrInput = 23;
+                secInput = 59;
+                minInput = 59;
+
+                // days = days * 60 * 60 - (remainingTime.inSeconds);
+                // days = (days / 60) as int;
 
                 // secInput = 0;
                 // hrInput = 24;
@@ -70,7 +80,12 @@ class _MyHomePageState extends State<MyHomePage> {
                 // sec = DateTime.now().second;
               });
             },
-          )
+          ),
+          ElevatedButton(
+              onPressed: () {
+                // audioInput.play(AssetSource("/audios/audio.wav"));
+              },
+              child: Text("play"))
         ],
       )),
     );
