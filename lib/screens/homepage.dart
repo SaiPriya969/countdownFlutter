8c273af713d77769c47eaa3d246b996c86797f02
sai <sai@example.com>
2023-03-20 11:03:25 +0530

    countdown

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,9 +1,12 @@
 // import 'dart:async';
 
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_countdown_timer/countdown_timer_controller.dart';
 import 'package:flutter_countdown_timer/flutter_countdown_timer.dart';
-// import 'package:audioplayers/audioplayers.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter_countdown_timer/index.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, title});
@@ -14,30 +17,61 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   late CountdownTimerController controller;
-  int hrInput = 14;
-  int minInput = 37;
+  int hrInput = 10;
+  int minInput = 52;
   int secInput = 00;
-  int displayhr = 14;
-  int displaymin = 37;
+  int displayhr = 10;
+  int displaymin = 52;
   int displaysec = 00;
-  int hr = DateTime.now().hour;
-  int min = DateTime.now().minute;
-  int sec = DateTime.now().second;
+  // int hr = DateTime.now().hour;
+  // int min = DateTime.now().minute;
+  // int sec = DateTime.now().second;
+  AudioPlayer audio = AudioPlayer();
+  loadAudio() {
+    print("entered");
+    return audio
+        .play("https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3");
+  }
+
+  DateTime getCurrentTime() {
+    int currentHour = DateTime.now().hour;
+
+    int currentMinute = DateTime.now().minute;
+    int currentSecond = DateTime.now().second;
+    DateTime currentTime = DateTime.now().add(Duration(
+        hours: currentHour, minutes: currentMinute, seconds: currentSecond));
+    print(currentTime);
+    print(DateTime.now());
+    return currentTime;
+  }
 
   int days = 0;
   Duration? rem;
   Duration remainingTime = Duration(hours: 0);
   void initState() {
     super.initState();
-    DateTime currentDuration =
-        DateTime.now().add(Duration(hours: hr, minutes: min, seconds: sec));
+    DateTime currentDuration = getCurrentTime();
+    // DateTime.now().add(Duration(hours: hr, minutes: min, seconds: sec));
     //currentDuration to currentTime
+    DateTime inputTime =
+        DateTime(DateTime.now().day, hrInput, minInput, secInput);
+    if (inputTime.isBefore(DateTime.now())) {
+      days = 1;
+    }
     DateTime inputDuration = DateTime.now().add(Duration(
         days: days, hours: hrInput, minutes: minInput, seconds: secInput));
-    // int remainingTime = inputDuration - currentDuration.inSeconds;
-    // if(inputDuration.difference(currentDuration)){
     remainingTime = inputDuration.difference(currentDuration);
-    // }
+
+    CountdownTimerController controller = CountdownTimerController(
+      endTime: DateTime.now().millisecondsSinceEpoch +
+          remainingTime.inSeconds * 1000,
+    );
+    // controller.addListener(() {
+    //   print("jj");
+    //   if (controller.currentRemainingTime!.min! <= 2) {
+    //     loadAudio();
+    //   }
+    // });
   }
 
   @override
@@ -49,9 +83,14 @@ class _MyHomePageState extends State<MyHomePage> {
     //     inputDuration.hour * 60 * 60);
     // print(currentDuration.second);
     // remainingTime=rem;
-    print(remainingTime);
+    // print(remainingTime);
     // based on remainingTime.. (+ or -) resturn the scaffold
 
+    // controller.addListener(() {
+    //   if (controller.currentRemainingTime!.min! <= 2) {
+    //     loadAudio();
+    //   }
+    // });
     return Scaffold(
       body: Center(
           child: Column(
@@ -61,8 +100,11 @@ class _MyHomePageState extends State<MyHomePage> {
             endTime: DateTime.now().millisecondsSinceEpoch +
                 remainingTime.inSeconds * 1000,
             // DateTime.now().millisecondsSinceEpoch + 9000,
+            // controller: controller,
             textStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+
             onEnd: () {
+              loadAudio();
               // print("hihr");
               setState(() {
                 // inputDuration.add(Duration(days: 1));
@@ -73,6 +115,16 @@ class _MyHomePageState extends State<MyHomePage> {
                 remainingTime = Duration(hours: 23, minutes: 59, seconds: 59);
                 // days = days * 60 * 60 - (remainingTime.inSeconds);
                 // days = (days / 60) as int;
+                // DateTime currentTime = DateTime.now();
+                // DateTime inputTime = DateTime(
+                //     currentTime.year,
+                //     currentTime.month,
+                //     currentTime.day,
+                //     hrInput,
+                //     minInput,
+                //     secInput);
+                // DateTime inputDuration = inputTime.add(Duration(days: days));
+                // remainingTime = inputDuration.difference(currentTime);
 
                 // secInput = 0;
                 // hrInput = 24;
@@ -82,11 +134,12 @@ class _MyHomePageState extends State<MyHomePage> {
                 // min = DateTime.now().minute;
                 // sec = DateTime.now().second;
               });
-            },
+            }, //onEnd
           ),
           ElevatedButton(
               onPressed: () {
                 // audioInput.play(AssetSource("/audios/audio.wav"));
+                loadAudio();
               },
               child: Text("play"))
         ],
